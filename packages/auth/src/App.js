af5f6f9213ddbde3c5d1378825dc74209c9b7c8a
Loreta Krasteva <52462274+loretta-arineva@users.noreply.github.com>
2022-06-21 11:08:14 +0300

    Add navigation and authentication

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -9,7 +9,7 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'au'
+  productionPrefix: 'au',
 });
 
 export default ({ history, onSignIn }) => {
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -12,7 +12,7 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
+  productionPrefix: 'co'
 });
 
 export default () => {
