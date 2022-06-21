b11fc3c8ebb4820e837a7a1513666937f4c8b752
Loreta Krasteva <52462274+loretta-arineva@users.noreply.github.com>
2022-06-21 11:02:02 +0300

    Add navigation and authentication

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,21 +1,22 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
-      onNavigate: ({ pathname: newPathname }) => {
-        const { pathname } = history.location
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
 
-        if (pathname !== newPathname) {
-          history.push(newPathname);
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
         }
-      }
+      },
+      onSignIn
     });
 
     history.listen(onParentNavigate);
